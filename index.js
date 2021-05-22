282a017a0cc8f9cda13421c8532b387dc3e60077
sbb3391 <sbb33391@gmail.com>
2021-05-22 10:38:52 -0400

    commit tree 1d5ad86a4407e268c54a3e9434ff9fd55a8a8708

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      const main = document.getElementsByTagName("body")[0];
+      
+      main.innerHTML += object.id;
+    })
+    .catch(function(error) {
+      const main = document.getElementsByTagName("body")[0];
+      alert("Something is amiss!");
+      main.innerHTML += error.message;
+    })
+}
